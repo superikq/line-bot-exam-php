2bcc08efe8c9fba6990c5b3597587f5c84fcdbf7
superikq <56840124+superikq@users.noreply.github.com>
2019-10-22 00:14:22 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'สวัสดีจ้า'
+				'text' => 'your id : ' . $text,
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
